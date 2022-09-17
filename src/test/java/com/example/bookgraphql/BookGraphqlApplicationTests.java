7b68d5d40b9c52fb6ddb3d2cb99054b48107cffc
mucio <muciojaziel@hotmail.com>
2022-09-17 15:54:16 -0300

    Inicio Projeto

---
+++ src/test/java/com/example/bookgraphql/BookGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
